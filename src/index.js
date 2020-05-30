48dde257c935dd69748f9b0c81edbb65345c7899
jb9229 <jb9229@gmail.com>
2020-05-30 13:32:59 +0900

    수정/ Edit expo update reload api

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ export default class App extends React.Component
       {
         await Updates.fetchUpdateAsync();
         // ... notify user of update ...
-        Updates.reloadFromCache();
+        Updates.reloadAsync();
         this.setState({ isAppUpdateComplete: true });
       }
 
