92f42f8e2dd29cf1a9970092ed6fd7b3fb7c2ba3
jb9229 <jb9229@gmail.com>
2020-10-11 11:20:18 +0900

    구현/웹/ playstore 연결

--- src/index.web.tsx
+++ src/index.web.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from '@apollo/client';
 import ApolloWebTest from './ApolloTest';
 import GPSSearchScreen from 'screens/GPSSearchScreen';
 import JBTerm from './components/templates/JBTerm';
+import MobileStoreButton from 'react-mobile-store-button';
 import { apolloClient } from 'src/api/apollo';
 import styled from 'styled-components/native';
 
@@ -16,10 +17,17 @@ const EquipmentSearchWrap = styled.View`
   height: 500px;
   width: 100%;
 `;
+const StoreLinkWrap = styled.View`
+  position: sticky;
+  width: 100%;
+  bottom: 10px;
+  right: 10px;
+`;
 
 interface Props {}
 const WebApp: React.FC<Props> = (props) =>
 {
+  const iOSUrl = 'https://play.google.com/store/apps/details?id=com.kan.jangbeecall';
   console.log(props);
   return (
     <ApolloProvider client={apolloClient}>
@@ -29,6 +37,13 @@ const WebApp: React.FC<Props> = (props) =>
           <GPSSearchScreen />
         </EquipmentSearchWrap>
         <JBTerm />
+        <StoreLinkWrap>
+          <MobileStoreButton
+            store="android"
+            url={iOSUrl}
+            linkProps={{ title: 'iOS Store Button' }}
+          />
+        </StoreLinkWrap>
       </Container>
     </ApolloProvider>
   );
