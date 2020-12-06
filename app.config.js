72a4ff206f6c92db267817011199463017405dcf
jb9229 <jb9229@gmail.com>
2020-12-06 13:43:14 +0900

    style: husky, eslint, prettier

--- app.config.js
+++ app.config.js
@@ -2,18 +2,25 @@ import 'dotenv/config';
 
 import { loadingStorybook } from './App';
 
-export default ({ config }) =>
-{
+export default ({ config }) => {
   console.log('>>> SLUG config: ', process.env.BUILD_TYPE);
-  console.log('>>> MY_CUSTOM_PROJECT_VERSION: ', process.env.MY_CUSTOM_PROJECT_VERSION);
+  console.log(
+    '>>> MY_CUSTOM_PROJECT_VERSION: ',
+    process.env.MY_CUSTOM_PROJECT_VERSION
+  );
   return {
     ...config,
     extra: {
-      buildType: process.env.BUILD_TYPE
+      buildType: process.env.BUILD_TYPE,
     },
     version: process.env.MY_CUSTOM_PROJECT_VERSION || '0.0.0',
-    slug: loadingStorybook ? 'jangbeecall_story' : process.env.BUILD_TYPE === 'dev' ? 'jangbeecall_dev'
-      : process.env.BUILD_TYPE === 'beta' ? 'jangbeecall_beta' : config.slug,
+    slug: loadingStorybook
+      ? 'jangbeecall_story'
+      : process.env.BUILD_TYPE === 'dev'
+      ? 'jangbeecall_dev'
+      : process.env.BUILD_TYPE === 'beta'
+      ? 'jangbeecall_beta'
+      : config.slug,
     icon: loadingStorybook ? './assets/images/storyIcon_32.png' : config.icon,
     hooks: {
       ...config.hooks,
@@ -23,10 +30,10 @@ export default ({ config }) =>
           config: {
             organization: 'Kan',
             project: 'jangbeecall',
-            authToken: process.env.SENTRY_AUTHTOKEN
-          }
-        }
-      ]
-    }
+            authToken: process.env.SENTRY_AUTHTOKEN,
+          },
+        },
+      ],
+    },
   };
 };
